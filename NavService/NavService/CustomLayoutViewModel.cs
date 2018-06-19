67c7e8a20ea5ed1c63f06f2ff0475b070ea443ba
john kenneth furog <furogjohnkenneth@gmail.com>
2018-06-19 19:53:45 +0800

    Add project files.

---
+++ NavService/NavService/CustomLayoutViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace NavService
+{
+    class CustomLayoutViewModel
+    {
+        public ObservableCollection<string> ImagesSources { get; } = new ObservableCollection<string>();
+
+        public CustomLayoutViewModel()
+        {
+            ImagesSources.Add("https://www.king.senate.gov/imo/media/image/2013%20Senator%20King%20Official%20Portrait1.jpg");
+            ImagesSources.Add("https://www.king.senate.gov/imo/media/image/2013%20Senator%20King%20Official%20Portrait1.jpg");
+            ImagesSources.Add("https://www.king.senate.gov/imo/media/image/2013%20Senator%20King%20Official%20Portrait1.jpg");
+            ImagesSources.Add("https://www.king.senate.gov/imo/media/image/2013%20Senator%20King%20Official%20Portrait1.jpg");
+            ImagesSources.Add("https://www.king.senate.gov/imo/media/image/2013%20Senator%20King%20Official%20Portrait1.jpg");
+        }
+    }
+}
